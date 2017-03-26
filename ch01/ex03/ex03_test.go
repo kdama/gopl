d43c011ad8ecf6416e76d8244846f0702299e794
Kai Kodama <cai.codama@gmail.com>
2017-03-26 16:30:52 +0900

    Run benchmark with 'go test'

---
+++ ch01/ex03/ex03_test.go
@@ -0,0 +1,23 @@
+package ex03
+
+import (
+	"testing"
+)
+
+func BenchmarkEcho1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		echo1()
+	}
+}
+
+func BenchmarkEcho2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		echo2()
+	}
+}
+
+func BenchmarkEcho3(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		echo3()
+	}
+}
