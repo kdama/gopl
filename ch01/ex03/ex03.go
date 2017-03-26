1455dbdf5c9e9cc3a334473a635a6fbd8aaae64c
Kai Kodama <cai.codama@gmail.com>
2017-03-26 16:31:14 +0900

    Run benchmark with 'go test' (#8)

---
+++ ch01/ex03/ex03.go
@@ -0,0 +1,29 @@
+package ex03
+
+import (
+	"os"
+	"strings"
+)
+
+func echo1() {
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	// fmt.Println(s)
+}
+
+func echo2() {
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	// fmt.Println(s)
+}
+
+func echo3() {
+	strings.Join(os.Args[1:], " ")
+	// fmt.Println(strings.Join(os.Args[1:], " "))
+}
