6e15b1bc2ea8d3b37f6a879379a92bfe3b31801f
Kai Kodama <cai.codama@gmail.com>
2017-04-08 22:08:00 +0900

    Add ch02-ex03 exercise

---
+++ ch02/ex03/README.md
@@ -0,0 +1,8 @@
+# `testall.bash`
+
+ベンチマークの結果は、次の通りです。
+
+```
+BenchmarkPopCount-2                     2000000000               0.77 ns/op
+BenchmarkAlternativePopCount-2          10000000               248 ns/op
+```
---
+++ ch02/ex03/popcount_test.go
@@ -0,0 +1,57 @@
+package popcount
+
+import (
+	"testing"
+)
+
+func TestPopCount(t *testing.T) {
+	var tests = []struct {
+		x    uint64
+		want int
+	}{
+		{0, 0},
+		{1, 1},
+		{1 << 8, 1},
+		{1<<8 + 1, 2},
+		{1<<8 - 1, 8},
+		{1<<64 - 1, 64},
+	}
+
+	for _, test := range tests {
+		if got := PopCount(test.x); got != test.want {
+			t.Errorf("PopCount(%d) = %d, want %d", test.x, got, test.want)
+		}
+	}
+}
+
+func TestAlternativePopCount(t *testing.T) {
+	var tests = []struct {
+		x    uint64
+		want int
+	}{
+		{0, 0},
+		{1, 1},
+		{1 << 8, 1},
+		{1<<8 + 1, 2},
+		{1<<8 - 1, 8},
+		{1<<64 - 1, 64},
+	}
+
+	for _, test := range tests {
+		if got := AlternativePopCount(test.x); got != test.want {
+			t.Errorf("AlternativePopCount(%d) = %d, want %d", test.x, got, test.want)
+		}
+	}
+}
+
+func BenchmarkPopCount(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCount(0)
+	}
+}
+
+func BenchmarkAlternativePopCount(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		AlternativePopCount(0)
+	}
+}
