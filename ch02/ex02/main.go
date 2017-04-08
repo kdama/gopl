a8e7950ed53f1284c603446f294dfc2bd2083715
Kai Kodama <cai.codama@gmail.com>
2017-04-08 21:14:37 +0900

    Add ch02-ex02 exercise (#30)

---
+++ ch02/ex02/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/kdama/gopl/ch02/ex02/conv"
+)
+
+func main() {
+	if len(os.Args) > 1 {
+		fromArgs()
+	} else {
+		fromStdin()
+	}
+}
+
+func fromArgs() {
+	for _, arg := range os.Args[1:] {
+		run(arg)
+	}
+}
+
+func fromStdin() {
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		run(s.Text())
+	}
+	if s.Err() != nil {
+		fmt.Fprintf(os.Stderr, "conv: %v\n", s.Err())
+		os.Exit(1)
+	}
+}
+
+func run(str string) {
+	value, unit, err := conv.Parse(str)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "conv: %s\n", err)
+		os.Exit(1)
+	}
+	from, to, err := conv.Convert(value, unit)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "conv: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("%s = %s\n", from, to)
+}
