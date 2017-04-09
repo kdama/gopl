0b93944746721e994b134bd6c2153e922c4cec0d
Kai Kodama <cai.codama@gmail.com>
2017-04-09 22:57:52 +0900

    Add comments to ch01-ex08

--- ch01/ex08/main.go
+++ ch01/ex08/main.go
@@ -1,3 +1,5 @@
+// ex08 は、URL にある内容を表示します。
+// URL に接頭辞 http:// がない場合は、追加してから内容を取得します。
 package main
 
 import (
@@ -26,6 +28,8 @@ func main() {
 	}
 }
 
+// appendPrefix は、与えられた文字列が接頭辞 http:// を持たなければ、それを追加した文字列を返します。
+// 既に接頭辞 http:// を持っていれば、与えられた文字列をそのまま返します。
 func appendPrefix(str string) string {
 	if !strings.HasPrefix(str, "http://") {
 		return "http://" + str
