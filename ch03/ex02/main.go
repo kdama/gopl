e64ef25688b8f5b5068b0244a979c88dec307703
Kai Kodama <cai.codama@gmail.com>
2017-05-03 16:03:56 +0900

    Add ch03-ex02 exercise (#51)

---
+++ ch03/ex02/main.go
@@ -0,0 +1,72 @@
+// ch03/ex02 は、鶏卵の箱のようなものの SVG レンダリングを計算します。
+// 計算には式 pow(2, sin(y)) * pow(2, sin(x)) / 12 を用います。
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const (
+	width, height = 600, 320            // canvas size in pixels
+	cells         = 100                 // number of grid cells
+	xyrange       = 30.0                // axis ranges (-xyrange..+xyrange)
+	xyscale       = width / 2 / xyrange // pixels per x or y unit
+	zscale        = height * 0.4        // pixels per z unit
+	angle         = math.Pi / 6         // angle of x, y axes (=30°)
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
+
+func main() {
+	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>", width, height)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay := corner(i+1, j)
+			bx, by := corner(i, j)
+			cx, cy := corner(i, j+1)
+			dx, dy := corner(i+1, j+1)
+
+			// 出力する前に、全ての値が有限かどうかを調べます。
+			if isFinite(ax) && isFinite(ay) &&
+				isFinite(bx) && isFinite(by) &&
+				isFinite(cx) && isFinite(cy) &&
+				isFinite(dx) && isFinite(dy) {
+				fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+					ax, ay, bx, by, cx, cy, dx, dy)
+			}
+		}
+	}
+	fmt.Println("</svg>")
+}
+
+func corner(i, j int) (float64, float64) {
+	// Find point (x,y) at corner of cell (i,j).
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+
+	// Compute surface height z.
+	z := f(x, y)
+
+	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
+	sx := width/2 + (x-y)*cos30*xyscale
+	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+	return sx, sy
+}
+
+func f(x, y float64) float64 {
+	return math.Pow(2, math.Sin(y)) * math.Pow(2, math.Sin(x)) / 12
+}
+
+// isFinite は、f が有限の値かどうかを返します。
+func isFinite(f float64) bool {
+	if math.IsInf(f, 0) {
+		return false
+	}
+	if math.IsNaN(f) {
+		return false
+	}
+	return true
+}
