5f319c8a8167012992b69e0356f0332e02b73af6
Kai Kodama <cai.codama@gmail.com>
2017-05-03 13:55:47 +0900

    Add ch03/ex01 exercise

---
+++ ch03/ex01/main.go
@@ -0,0 +1,85 @@
+// ch03/ex01 は、不正なポリゴンをスキップしながら、3-D 面の関数の SVG レンダリングを計算します。
+// 関数 f は、無限大の値や NaN を返すことがあるように変更されています。
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const (
+	width, height = 600, 320            // canvas size in pixels
+	cells         = 100                 // number of grid cells
+	xyrange       = 30.0                // axis ranges (-xyrange..+xyrange)
+	xyscale       = width / 2 / xyrange // pixels per x or y unit
+	zscale        = height * 0.4        // pixels per z unit
+	angle         = math.Pi / 6         // angle of x, y axes (=30°)
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
+
+func main() {
+	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>", width, height)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay := corner(i+1, j)
+			bx, by := corner(i, j)
+			cx, cy := corner(i, j+1)
+			dx, dy := corner(i+1, j+1)
+
+			// 出力する前に、全ての値が有限かどうかを調べます。
+			if isFinite(ax) && isFinite(ay) &&
+				isFinite(bx) && isFinite(by) &&
+				isFinite(cx) && isFinite(cy) &&
+				isFinite(dx) && isFinite(dy) {
+				fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+					ax, ay, bx, by, cx, cy, dx, dy)
+			}
+		}
+	}
+	fmt.Println("</svg>")
+}
+
+func corner(i, j int) (float64, float64) {
+	// Find point (x,y) at corner of cell (i,j).
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+
+	// Compute surface height z.
+	z := f(x, y)
+
+	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
+	sx := width/2 + (x-y)*cos30*xyscale
+	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+	return sx, sy
+}
+
+func f(x, y float64) float64 {
+	r := math.Hypot(x, y) // distance from (0,0)
+
+	// 動作確認のために、r がある範囲のとき、無限大の値や NaN を返します。
+	if r >= 10 && r < 12 {
+		return math.Inf(0)
+	}
+	if r >= 14 && r < 16 {
+		return math.Inf(-1)
+	}
+	if r >= 18 && r < 20 {
+		return math.NaN()
+	}
+
+	return math.Sin(r) / r
+}
+
+// isFinite は、f が有限の値かどうかを返します。
+func isFinite(f float64) bool {
+	if math.IsInf(f, 0) {
+		return false
+	}
+	if math.IsNaN(f) {
+		return false
+	}
+	return true
+}
