1311dd6e9a7d5d5476a0894a7774038d4d4df7cd
Kai Kodama <cai.codama@gmail.com>
2017-05-03 16:03:43 +0900

    Modify output of ch03-ex01 runall script (#50)

--- ch03/ex01/runall.bash
+++ ch03/ex01/runall.bash
@@ -4,7 +4,7 @@ go run main.go > out/surface.svg
 
 # Verify that surface.svg does not include 'Inf' nor 'NaN'.
 if grep -q -e "Inf" -e "NaN" out/surface.svg; then
-  echo "FAIL: surface.svg includes 'Inf' or 'NaN'."
+  echo "[FAIL] surface.svg includes 'Inf' or 'NaN'."
 else
-  echo "SUCCESS: surface.svg does not include 'Inf' nor 'NaN'."
+  echo "[SUCCESS] surface.svg does not include 'Inf' nor 'NaN'."
 fi
