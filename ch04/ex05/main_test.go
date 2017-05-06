f7adcece70809bae896ad1f97b602fbf3244c4b6
Kai Kodama <cai.codama@gmail.com>
2017-05-07 00:33:11 +0900

    Add ch04/ex05 (#75)

---
+++ ch04/ex05/main_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"testing"
+)
+
+func equals(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func TestRemoveDup(t *testing.T) {
+	var tests = []struct {
+		s, scopy []string
+		want     []string
+	}{
+		{[]string{}, []string{}, []string{}},
+		{[]string{"A"}, []string{"A"}, []string{"A"}},
+		{[]string{"A", "A"}, []string{"A", "A"}, []string{"A"}},
+		{[]string{"A", "A", "B", "B", "A", "C"}, []string{"A", "A", "B", "B", "A", "C"}, []string{"A", "B", "A", "C"}},
+	}
+
+	for _, test := range tests {
+		got := removeDup(test.s)
+		if !equals(got, test.want) {
+			t.Errorf("reverse(%v) = %v, want %v", test.scopy, got, test.want)
+		}
+	}
+}
