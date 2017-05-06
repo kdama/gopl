34ab1585ca6c659440123f1242596918d98f8a48
Kai Kodama <cai.codama@gmail.com>
2017-05-07 01:09:40 +0900

    Add ch04/ex06 (#77)

---
+++ ch04/ex06/main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestRemoveDupSpace(t *testing.T) {
+	var tests = []struct {
+		b    []byte
+		want []byte
+	}{
+		{[]byte(""), []byte("")},
+		{[]byte("A"), []byte("A")},
+		{[]byte("A B C"), []byte("A B C")},
+		{[]byte("A  B  C"), []byte("A B C")},
+		{[]byte("AB C  D   "), []byte("AB C D ")},
+		{[]byte("   A  B CD"), []byte(" A B CD")},
+	}
+
+	for _, test := range tests {
+		got := removeDupSpace(test.b)
+		if string(got) != string(test.want) {
+			t.Errorf("removeDupSpace(%q) = %q, want %q", string(test.b), string(got), string(test.want))
+		}
+	}
+}
