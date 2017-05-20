135245e6d261f6cfcc114cdb7a01a80b58553387
Kai Kodama <cai.codama@gmail.com>
2017-05-20 14:05:34 +0900

    Rename directory for linguist (#104)

--- ch05/ex01/runall.bash
+++ ch05/ex01/runall.bash
@@ -4,5 +4,5 @@
 # go build gopl.io/ch1/fetch
 # ./fetch https://golang.org | go run main.go
 
-go run main.go < in/in.html > out/out.txt
-diff expected/out.txt out/out.txt
+go run main.go < examples/in.html > out/out.txt
+diff examples/out.txt out/out.txt
