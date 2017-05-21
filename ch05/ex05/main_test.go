51a6ae38d22dc42e0e0ef1aa77c26e6e845dd752
Kai Kodama <cai.codama@gmail.com>
2017-05-21 16:53:15 +0900

    Add ch05/ex05 (#110)

---
+++ ch05/ex05/main_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"testing"
+
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func TestCountWordsAndImages(t *testing.T) {
+	var tests = []struct {
+		html string
+		want struct {
+			words  int
+			images int
+		}
+	}{
+		{"", struct {
+			words  int
+			images int
+		}{words: 0, images: 0}},
+		{"<p>foo</p>", struct {
+			words  int
+			images int
+		}{words: 1, images: 0}},
+		{"<img src='http://example.com/src.png'>", struct {
+			words  int
+			images int
+		}{words: 0, images: 1}},
+		{"<script>foo</script>", struct {
+			words  int
+			images int
+		}{words: 0, images: 0}},
+		{"<p>Hello, world.</p><img src='src.png'><p>こんにちは<br>世界</p>", struct {
+			words  int
+			images int
+		}{words: 4, images: 1}},
+		{"<p>Hello,world.</p><img src='src.png'><p>こんにちは世界</p>", struct {
+			words  int
+			images int
+		}{words: 2, images: 1}},
+	}
+
+	for _, test := range tests {
+		doc, err := html.Parse(strings.NewReader(test.html))
+		if err != nil {
+			t.Errorf("failed to parse HTML: %s", test.html)
+		}
+
+		words, images := countWordsAndImages(doc)
+
+		if words != test.want.words {
+			t.Errorf("words of countWordsAndImages of %q = %d, want %d", test.html, words, test.want.words)
+		}
+		if images != test.want.images {
+			t.Errorf("images of countWordsAndImages of %q = %d, want %d", test.html, images, test.want.images)
+		}
+	}
+}
