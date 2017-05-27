fc628f890e0807e093e7a03255af90bc054dcd15
Kai Kodama <cai.codama@gmail.com>
2017-05-27 22:23:03 +0900

    Add ch06/ex02 (#129)

---
+++ ch06/ex02/intset/intset.go
@@ -0,0 +1,70 @@
+// Package intset は、ビットベクタベースの整数のセットを提供します。
+package intset
+
+import (
+	"bytes"
+	"fmt"
+)
+
+// IntSet は、負ではない小さな整数のセットです。そのゼロ値は空セットを表しています。
+type IntSet struct {
+	words []uint64
+}
+
+// Has は、負ではない値 x をセットが含んでいるか否かを報告します。
+func (s *IntSet) Has(x int) bool {
+	word, bit := x/64, uint(x%64)
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+}
+
+// Add はセットに負ではない値 x を追加します。
+func (s *IntSet) Add(x int) {
+	word, bit := x/64, uint(x%64)
+	for word >= len(s.words) {
+		s.words = append(s.words, 0)
+	}
+	s.words[word] |= 1 << bit
+}
+
+// UnionWith は、s と t の和集合を s に設定します。
+func (s *IntSet) UnionWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] |= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+// String は、"{1 2 3}" の形式の文字列としてセットを返します。
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				fmt.Fprintf(&buf, "%d", 64*i+j)
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
+}
+
+// AddAll は、セットに負ではない複数の値を追加します。
+func (s *IntSet) AddAll(vals ...int) {
+	for _, x := range vals {
+		word, bit := x/64, uint(x%64)
+		for word >= len(s.words) {
+			s.words = append(s.words, 0)
+		}
+		s.words[word] |= 1 << bit
+	}
+}
