78118d6375e9847b8c2ba136ae7d519dbf137675
Kai Kodama <cai.codama@gmail.com>
2017-05-27 23:05:02 +0900

    Add ch06/ex03

---
+++ ch06/ex03/main.go
@@ -0,0 +1,39 @@
+// ch06/ex01 は、IntSet のいくつかの関数を実行します。
+package main
+
+import (
+	"fmt"
+
+	"github.com/kdama/gopl/ch06/ex03/intset"
+)
+
+func main() {
+	s := &intset.IntSet{}
+	t := &intset.IntSet{}
+
+	s.Add(1)
+	s.Add(2)
+	s.Add(3)
+
+	t.Add(1)
+	t.Add(4)
+
+	fmt.Println(s) // "{1 2 3}"
+
+	s.IntersectWith(t)
+	fmt.Println(s) // "{1}"
+
+	s.Add(1)
+	s.Add(2)
+	s.Add(3)
+
+	s.DifferenceWith(t)
+	fmt.Println(s) // "{2 3}"
+
+	s.Add(1)
+	s.Add(2)
+	s.Add(3)
+
+	s.SymmetricDifference(t)
+	fmt.Println(s) // "{2 3 4}"
+}
