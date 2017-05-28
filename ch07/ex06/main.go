ac25c3fd21a2e438b38741da190ae2a511cbaf07
Kai Kodama <cai.codama@gmail.com>
2017-05-28 18:06:29 +0900

    Add ch07/ex07 (#139)
    * Add support for Kelvin
    
    * Add ch07/ex07

--- ch07/ex06/main.go
+++ ch07/ex06/main.go
@@ -1,4 +1,4 @@
-// ch07/ex06 は、-temp フラグで指定された温度を、絶対温度で表示します。
+// ch07/ex06 は、-temp フラグで指定された温度を、摂氏で表示します。
 package main
 
 import (
@@ -8,7 +8,7 @@ import (
 	"github.com/kdama/gopl/ch07/ex06/tempconv"
 )
 
-var temp = tempconv.KelvinFlag("temp", 20.0, "the temperature")
+var temp = tempconv.CelsiusFlag("temp", 20.0, "the temperature")
 
 func main() {
 	flag.Parse()
--- ch07/ex07/tempconv/tempconv.go
+++ ch07/ex07/tempconv/tempconv.go
@@ -1,4 +1,5 @@
 // Package tempconv は、摂氏、華氏、および絶対温度の温度計算を行います。
+// ただし、Celsius や Fahrenheit が文字列として表示される際に ° を表示しません。
 package tempconv
 
 import (
@@ -12,35 +13,19 @@ type Celsius float64
 // Fahrenheit は、華氏の温度を表します。
 type Fahrenheit float64
 
-// Kelvin は、絶対温度を表します。
-type Kelvin float64
-
 const (
 	// AbsoluteZeroC は、絶対零度です。
 	AbsoluteZeroC Celsius = -273.15
 )
 
-func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) }
-func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
-func (k Kelvin) String() string     { return fmt.Sprintf("%gK", k) }
+func (c Celsius) String() string    { return fmt.Sprintf("%gC", c) }
+func (f Fahrenheit) String() string { return fmt.Sprintf("%gF", f) }
 
 // CToF は、摂氏の温度を華氏の温度に変換します。
 func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 
 // FToC は、華氏の温度を摂氏の温度に変換します。
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
-
-// CToK は、摂氏の温度を絶対温度に変換します。
-func CToK(c Celsius) Kelvin { return Kelvin(c - AbsoluteZeroC) }
-
-// KToC は、絶対温度を摂氏の温度に変換します。
-func KToC(k Kelvin) Celsius { return Celsius(k + Kelvin(AbsoluteZeroC)) }
-
-// FToK は、華氏の温度を絶対温度に変換します。
-func FToK(f Fahrenheit) Kelvin { return CToK(FToC(f)) }
-
-// KToF は、絶対温度を華氏の温度に変換します。
-func KToF(k Kelvin) Fahrenheit { return CToF(KToC(k)) }
 
 type celsiusFlag struct{ Celsius }
 
@@ -54,9 +39,6 @@ func (f *celsiusFlag) Set(s string) error {
 		return nil
 	case "F", "°F":
 		f.Celsius = FToC(Fahrenheit(value))
-		return nil
-	case "K":
-		f.Celsius = KToC(Kelvin(value))
 		return nil
 	}
 	return fmt.Errorf("invalid temperature %q", s)
