843646e58f69a2dea4eac2aecb3868934b85623d
Kai Kodama <cai.codama@gmail.com>
2017-06-03 20:15:02 +0900

    Add ch07/ex14 (#150)

---
+++ ch07/ex14/eval/string_test.go
@@ -0,0 +1,34 @@
+package eval
+
+import (
+	"testing"
+)
+
+func TestString(t *testing.T) {
+	tests := []struct {
+		expr string
+	}{
+		{"sqrt(A / pi) ? sqrt(A / pi) : pi"},
+		{"pow(x, 3) + pow(y, 3) ? pow(x, 3) : pow(y, 3)"},
+		{"5 ? 9 : 5 / 9 * (F - 32)"},
+		{"-1 ? -1 + -x : -x"},
+		{"-x ? -1 : -1 - x"},
+	}
+	for _, test := range tests {
+		expr, err := Parse(test.expr)
+		if err != nil {
+			t.Errorf("%s: %v", test.expr, err) // parse error
+			continue
+		}
+
+		got, err := Parse(expr.String())
+		if err != nil {
+			t.Error(err) // parse error
+			continue
+		}
+
+		if !expr.Equals(got) {
+			t.Errorf("Parse(expr) != Parse(Parse(expr)): expr = %s", test.expr)
+		}
+	}
+}
