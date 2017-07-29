214039b862f45f5ed820cd05557a7e75b87217c7
Kai Kodama <cai.codama@gmail.com>
2017-07-29 21:18:59 +0900

    Add ch11/ex04

---
+++ ch11/ex04/word/word_test.go
@@ -0,0 +1,42 @@
+package word
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+var puncspaces = []rune{
+	' ', ',', '.', '!', '?', '　', '、', '。', '！', '？',
+}
+
+func randomPalindrome(rng *rand.Rand) string {
+	n := rng.Intn(25) // random length up to 24
+	runes := make([]rune, n)
+	for i := 0; i < (n+1)/2; i++ {
+		r := rune(rng.Intn(0x1000)) // random rune up to '\u0999'
+		runes[i] = r
+		runes[n-1-i] = r
+	}
+
+	if n == 0 {
+		return string(runes)
+	}
+	pos := rng.Intn(n)
+	runes = append(runes[:pos], append([]rune{puncspaces[rng.Intn(len(puncspaces))]}, runes[pos:]...)...)
+	return string(runes)
+}
+
+func TestRandomPalindromes(t *testing.T) {
+	// Initialize a pseudo-random number generator.
+	seed := time.Now().UTC().UnixNano()
+	t.Logf("Random seed: %d", seed)
+	rng := rand.New(rand.NewSource(seed))
+
+	for i := 0; i < 1000; i++ {
+		p := randomPalindrome(rng)
+		if !IsPalindrome(p) {
+			t.Errorf("IsPalindrome(%q) = false", p)
+		}
+	}
+}
