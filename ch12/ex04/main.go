a737efd0ac34fcc719be355a05db22a339cede39
Kai Kodama <cai.codama@gmail.com>
2017-09-09 18:16:25 +0100

    Add ch12/ex03 and ch12/ex04 (#207)

---
+++ ch12/ex04/main.go
@@ -0,0 +1,55 @@
+// ch12/ex04 は、Go の値を S 式形式でエンコードして、プリティプリントします。
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+
+	"github.com/kdama/gopl/ch12/ex03/sexpr"
+)
+
+func main() {
+	type Movie struct {
+		Title, Subtitle string
+		Year            int
+		Actor           map[string]string
+		Oscars          []string
+		Sequel          *string
+		True            bool
+		False           bool
+		Float           float64
+		Complex         complex128
+		Interface       interface{}
+	}
+	strangelove := Movie{
+		Title:    "Dr. Strangelove",
+		Subtitle: "How I Learned to Stop Worrying and Love the Bomb",
+		Year:     1964,
+		Actor: map[string]string{
+			"Dr. Strangelove":            "Peter Sellers",
+			"Grp. Capt. Lionel Mandrake": "Peter Sellers",
+			"Pres. Merkin Muffley":       "Peter Sellers",
+			"Gen. Buck Turgidson":        "George C. Scott",
+			"Brig. Gen. Jack D. Ripper":  "Sterling Hayden",
+			`Maj. T.J. "King" Kong`:      "Slim Pickens",
+		},
+		Oscars: []string{
+			"Best Actor (Nomin.)",
+			"Best Adapted Screenplay (Nomin.)",
+			"Best Director (Nomin.)",
+			"Best Picture (Nomin.)",
+		},
+		True:      true,
+		False:     false,
+		Float:     math.Pi,
+		Complex:   complex(1, 2),
+		Interface: []int{1, 2, 3},
+	}
+
+	data, err := sexpr.MarshalIndent(strangelove)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(data))
+}
